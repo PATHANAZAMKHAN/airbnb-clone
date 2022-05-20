f6cda55cfadc2496b4a8393832c6de6b8a0327fb
PATHANAZAMKHAN <pathanazam001@gmail.com>
2022-05-20 21:50:49 +0530

    Final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-import {Navbar, Uppersection} from "./Upper-section";
+import { Navbar, Uppersection } from "./Upper-section";
 import Card from "./Lower-section";
 import Data from "./Data";
 
 function App() {
 
-  let cards =  Data.map((data)=>{
-    return(
-      <Card coverImg = {data.coverImg} title = {data.title} desc = {data.description} price ={parseInt(data.price)} rating = {parseFloat(data.stats.rating)} reviewCount = {parseInt(data.stats.reviewCount)} />
+  let cards = Data.map((data) => {
+    return (
+      <Card key={data.id} items = {data} />
     )
   })
 
   return (
     <>
       <Navbar />
-      <Uppersection/>
-      <div id = "app-cards">
+      <Uppersection />
+      <div id="app-cards">
         {cards}
       </div>
     </>
--- src/components/Lower-section.js
+++ src/components/Lower-section.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Star from "../Images/star.png";
-// import katie from "../Images/katie-zaferes.png"
 
-
-export default function Card(props){
-    return(
+export default function Card(props) {
+    let source = require(`../Images/${props.items.coverImg}`);
+    let badgeText
+    if (props.items.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.items.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    return (
         <div id="card">
-
-            <img src= "../Images/katie-zaferes.png" alt ="Error loading"/>   
-            <span><img src={Star} alt = "Error loading" id = "star"/> {props.rating}({props.reviewCount}) . Usa</span>
-            <p>{props.title}</p>
-            <span><strong>From ${props.price}</strong>/ person</span> 
+            <span id="sold-out">{badgeText}</span>
+            <img src={source} alt="Error loading" />
+            <span><img src={Star} alt="Error loading" id="star" /> {props.items.stats.rating}({props.items.stats.reviewCount}) . Usa</span>
+            <p>{props.items.title}</p>
+            <span><strong>From ${props.items.price}</strong>/ person</span>
 
         </div>
     )
